ea560a1cc3c2529ec33c81576351a62c37200343
Yue Zhao <yue.zhao@surf.nl>
2023-01-30 14:07:56 +0100

    update model to be compatible with sonnet2

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ from graph_nets import modules
 from functools import partial
 
 
-class EncodeProcessDecodeNonRecurrent(snt.AbstractModule):
+class EncodeProcessDecodeNonRecurrent(snt.Module):
     """
     similar to EncodeProcessDecode, but with non-recurrent core
     see docs for EncodeProcessDecode
@@ -22,29 +22,39 @@ class EncodeProcessDecodeNonRecurrent(snt.AbstractModule):
                  concat_encoder=True,
                  name="EncodeProcessDecodeNonRecurrent"):
         super(EncodeProcessDecodeNonRecurrent, self).__init__(name=name)
-        self._encoder = MLPGraphIndependent(latent_size=latent_size, num_layers=num_layers)
-        self._cores = [MLPGraphNetwork(latent_size=latent_size, num_layers=num_layers,
-                                       global_block=global_block) for _ in range(num_cores)]
-        self._decoder = MLPGraphIndependent(latent_size=latent_size, num_layers=num_layers)
-        self.concat_encoder = concat_encoder
+        self._latent_size = latent_size
+        self._num_layers = num_layers
+        self._global_block = global_block
+        self._concat_encoder = concat_encoder
+        self._edge_output_size = edge_output_size
+        self._node_output_size = node_output_size
+
+
+    def _initialize(self):
+        self._encoder = MLPGraphIndependent(self._latent_size, self._num_layers)
+        self._cores = [MLPGraphNetwork(latent_size=self._latent_size, num_layers=self._num_layers,
+                                       global_block=self._global_block) for _ in range(self._num_cores)]
+        self._decoder = MLPGraphIndependent(self._latent_size, self._num_layers)
+        self.concat_encoder = self._concat_encoder
         # Transforms the outputs into the appropriate shapes.
-        if edge_output_size is None:
+        if self._edge_output_size is None:
             edge_fn = None
         else:
-            edge_fn = lambda: snt.Linear(edge_output_size, name="edge_output")
-        if node_output_size is None:
+            edge_fn = lambda: snt.Linear(self._edge_output_size, name="edge_output")
+        if self._node_output_size is None:
             node_fn = None
         else:
-            node_fn = lambda: snt.Linear(node_output_size, name="node_output")
-        if global_output_size is None:
+            node_fn = lambda: snt.Linear(self._node_output_size, name="node_output")
+        if self._global_output_size is None:
             global_fn = None
         else:
-            global_fn = lambda: snt.Linear(global_output_size, name="global_output")
+            global_fn = lambda: snt.Linear(self._global_output_size, name="global_output")
         with self._enter_variable_scope():
             self._output_transform = modules.GraphIndependent(edge_fn, node_fn,
                                                               global_fn)
 
-    def _build(self, input_op):
+    def __call__(self, input_op):
+        self._initialize()
         latent = self._encoder(input_op)
         latent0 = latent
         for i in range(len(self._cores)):
@@ -56,68 +66,81 @@ class EncodeProcessDecodeNonRecurrent(snt.AbstractModule):
         return self._output_transform(self._decoder(latent))
 
 
-class MLPGraphNetwork(snt.AbstractModule):
+class MLPGraphNetwork(snt.Module):
     """GraphNetwork with MLP edge, node, and global models."""
 
-    def __init__(self, latent_size=16, num_layers=2, global_block=True, last_round=False,
+    def __init__(self,
+                 latent_size=16,
+                 num_layers=2,
+                 global_block=True,
+                 last_round=False,
                  name="MLPGraphNetwork"):
         super(MLPGraphNetwork, self).__init__(name=name)
-        partial_make_mlp_model = partial(make_mlp_model, latent_size=latent_size, num_layers=num_layers,
+        self._latent_size = latent_size
+        self._num_layers = num_layers
+        self._global_block = global_block
+        self._last_round = last_round
+
+
+    def _initialize(self):
+        partial_make_mlp_model = partial(make_mlp_model, self._latent_size, self._num_layers,
                                          last_round_edges=False)
-        if last_round:
-            partial_make_mlp_model_edges = partial(make_mlp_model, latent_size=latent_size, num_layers=num_layers,
+        if self._last_round:
+            partial_make_mlp_model_edges = partial(make_mlp_model, self._latent_size, self._num_layers,
                                                    last_round_edges=True)
         else:
             partial_make_mlp_model_edges = partial_make_mlp_model
-
-        with self._enter_variable_scope():
-            if global_block:
-                self._network = modules.GraphNetwork(partial_make_mlp_model_edges, partial_make_mlp_model,
-                                                     partial_make_mlp_model,
-                                                     edge_block_opt={
-                                                         "use_globals": True
-                                                     },
-                                                     node_block_opt={
-                                                         "use_globals": True
-                                                     },
-                                                     global_block_opt={
-                                                         "use_globals": True,
-                                                         "edges_reducer": tf.unsorted_segment_mean,
-                                                         "nodes_reducer": tf.unsorted_segment_mean
-                                                     })
-            else:
-                self._network = modules.GraphNetwork(partial_make_mlp_model_edges, partial_make_mlp_model,
-                                                     make_identity_model,
-                                                     edge_block_opt={
-                                                         "use_globals": False
-                                                     },
-                                                     node_block_opt={
-                                                         "use_globals": False
-                                                     },
-                                                     global_block_opt={
-                                                         "use_globals": False,
-                                                     })
-
-    def _build(self, inputs):
+        if self._global_block:
+            self._network = modules.GraphNetwork(partial_make_mlp_model_edges, partial_make_mlp_model,
+                                                 partial_make_mlp_model,
+                                                 edge_block_opt={
+                                                     "use_globals": True
+                                                 },
+                                                 node_block_opt={
+                                                     "use_globals": True
+                                                 },
+                                                 global_block_opt={
+                                                     "use_globals": True,
+                                                     "edges_reducer": tf.unsorted_segment_mean,
+                                                     "nodes_reducer": tf.unsorted_segment_mean
+                                                 })
+        else:
+            self._network = modules.GraphNetwork(partial_make_mlp_model_edges, partial_make_mlp_model,
+                                                 make_identity_model,
+                                                 edge_block_opt={
+                                                     "use_globals": False
+                                                 },
+                                                 node_block_opt={
+                                                     "use_globals": False
+                                                 },
+                                                 global_block_opt={
+                                                     "use_globals": False,
+                                                 })
+    def __call__(self, inputs):
+        self._initialize()
         return self._network(inputs)
 
 
-class MLPGraphIndependent(snt.AbstractModule):
+class MLPGraphIndependent(snt.Module):
     """GraphIndependent with MLP edge, node, and global models."""
 
-    def __init__(self, latent_size=16, num_layers=2, name="MLPGraphIndependent"):
+    def __init__(self,
+                 latent_size=16,
+                 num_layeres=2,
+                 name="MLPGraphIndependent"):
         super(MLPGraphIndependent, self).__init__(name=name)
+        self._latent_size = latent_size
+        self._num_layers = num_layeres
 
-        partial_make_mlp_model = partial(make_mlp_model, latent_size=latent_size, num_layers=num_layers,
+    def _initialize(self):
+        partial_make_mlp_model = partial(make_mlp_model, self._latent_size, self._num_layers,
                                          last_round_edges=False)
-
-        with self._enter_variable_scope():
-            self._network = modules.GraphIndependent(
-                edge_model_fn=partial_make_mlp_model,
-                node_model_fn=partial_make_mlp_model,
-                global_model_fn=partial_make_mlp_model)
-
-    def _build(self, inputs):
+        self._network = modules.GraphIndependent(
+            edge_model_fn=partial_make_mlp_model,
+            node_model_fn=partial_make_mlp_model,
+            global_model_fn=partial_make_mlp_model)
+    def __call__(self, inputs):
+        self._initialize()
         return self._network(inputs)
 
 
@@ -138,8 +161,8 @@ def make_mlp_model(latent_size=16, num_layers=2, last_round_edges=False):
         ])
 
 
-class IdentityModule(snt.AbstractModule):
-    def _build(self, inputs):
+class IdentityModule(snt.Module):
+    def __call__(self, inputs):
         return tf.identity(inputs)
 
 
